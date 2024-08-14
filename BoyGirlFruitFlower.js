b15845bb89913cd2115784860baa37ecaa54a59e
nathanparle <62899873+nathanparle@users.noreply.github.com>
2024-08-14 13:26:54 +0100

    Add files via upload

--- BoyGirlFruitFlower.js
+++ BoyGirlFruitFlower.js
@@ -7,9 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
             cell.contentEditable = true; 
             cell.addEventListener('click', () => {
                 cell.focus();
-            })
+            });
         }
     }
-}
+
+const generateLetterBtn = document.getElementById("generateLetterBtn");
+const randomLetterDisplay = document.getElementById("randomLetterDisplay");
+
+generateLetterBtn.addEventListener('click', () => {
+    const randomLetter = String.fromCharCode(97 + Math.floor(Math.random()*26));
+    randomLetterDisplay.textContent = `Random Letter: ${randomLetter}`;
+})}
 
 )
